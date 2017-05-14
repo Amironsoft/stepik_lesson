bdac16b1a77186a12a83cd2b2254ea82ae900312
Ams <amironsoft@mail.ru>
2017-05-14 23:48:31 +0300

    Update test for strange lesson id

--- test/test_stepik_http_server.py
+++ test/test_stepik_http_server.py
@@ -3,15 +3,26 @@ from app.stepik_http_server_adv import get_theor_steps
 
 
 class TestStepikServer(unittest.TestCase):
-    def test_server(self):
-        test_id_list = [24471,
+    def test_server_positive(self):
+        test_id_list_positive = [24471,
                         7627,
                         24813,
                         42713]
-        for lesson_id in test_id_list:
+        for lesson_id in test_id_list_positive:
             message, success_status = get_theor_steps(lesson_id)
             print(lesson_id, message, success_status)
             self.assertTrue(success_status)
+
+    def test_server_negative(self):
+        test_id_list_negative = [244711111,
+                                 7627777,
+                                 'some strange str',
+                                 ['{', 12312321312]]
+        for lesson_id in test_id_list_negative:
+            message, success_status = get_theor_steps(lesson_id)
+            print(lesson_id, message, success_status)
+            self.assertFalse(success_status)
+
 
 if __name__ == '__main__':
     unittest.main()
